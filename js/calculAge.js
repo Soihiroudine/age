bb3955ff2df2f55750db8ecf67c8b08ab3c74c69
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2024-10-21 11:52:26 +0300

    Deplacement des fonctions, mise en place de la fonction flecher

--- js/calculAge.js
+++ js/calculAge.js
@@ -1,4 +1,4 @@
-// declaration de variable des element qui vont etre recuperer dans le html
+// declaration de variable des elements qui vont etre recuperer dans le html
 let nom, prenom, categoriesPro;
 let annee = document.querySelector("#anneeNaissance");
 let boutton = document.querySelector("#button");
@@ -12,25 +12,20 @@ const anneeActuelle = new Date().getFullYear();
 let categories = ["Etudiant", "Salarier"];
 
 // On creer un element option
-function optionCategories(donnee){
+function optionCategories(donnee) {
     let option = document.createElement("option");
     option.setAttribute("value", donnee);
-    option.innerHTML = donnee;
+    // option.innerHTML = donnee;
     return option;
-}
-
-// La boucle va permettre d'ajouter les categories dans le code html
-for (let index = 0; index < categories.length; index++) {
-    valeurInputCategorie.appendChild(optionCategories(categories[index]));
 }
 
 // Fonction qui veirifie que la valeur rechercher est bien dans notre tableau
 // On va l'utiliser en premier lieu pour s'assurer que l'utilisateur a bien selectionner une valeur définit
 function valeurDansTableau(valeur, tableau) {
-   for(let index = 0; index > tableau.length; index++) {
-    if (tableau[index] === valeur) { return true; }
-   }
-   return false;
+    for (let index = 0; index > tableau.length; index++) {
+        if (tableau[index] === valeur) { return true; }
+    }
+    return false;
 }
 
 // calcul de l'age 
@@ -39,27 +34,16 @@ function calculAge(ans) {
 }
 
 // Creation d'une fonction
-// elle va créer un span
-function afficheAge(valeur) {
+// elle va créer un span et va lui mettre une valeur html
+function afficheSpan(valeur) {
     let span = document.querySelector("span");
     span.innerHTML = valeur;
     return span;
 }
 
-// La fonction qui s'occupe de l'affichage de l'age
-function afficheTexte() {
-    nom = document.querySelector("#nom").value;
-    prenom = document.querySelector("#prenom").value;
-    categoriesPro = document.querySelector("#categoriesPro").value;
-    annee = document.querySelector("#anneeNaissance").value;
-    let age = calculAge(annee);
-    let affichage = document.querySelector("#affichage p");
-    // console.log(calculAge(annee));
-    if (annee > anneeActuelle/*  || categoriesPro === "" || */ ) {
-        alert("Veuiller mettre une année inférieur à l'année actuelle");
-    } else {
-        affichage.appendChild(afficheAge(age));
-    }
+// La boucle va permettre d'ajouter les categories dans le code html
+for (let index = 0; index < categories.length; index++) {
+    valeurInputCategorie.appendChild(optionCategories(categories[index]));
 }
 
 // Declanchement de l'action affichage de l'age quand le bouton "entrer" est clicker
@@ -67,4 +51,13 @@ function afficheTexte() {
 
 // Quand on va mettre un nombre dans le champs input:number de l'annee 
 // il va automatiquement afficher l'age
-annee.addEventListener("change", afficheTexte);
+annee.addEventListener("change", () => {
+    annee = document.querySelector("#anneeNaissance");
+    let age = calculAge(annee.value);
+    let affichage = document.querySelector("#affichage p");
+    if (annee > anneeActuelle/*  || categoriesPro === "" || */) {
+        alert("Veuiller mettre une année inférieur à l'année actuelle");
+    } else {
+        affichage.appendChild(afficheSpan(age));
+    }
+});
