e5b863783cabbf1fb941d4ca5d075fa5101a0474
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2024-10-24 12:06:53 +0300

    Travail sur l'evenement change, date

--- js/calculAge.js
+++ js/calculAge.js
@@ -1,5 +1,5 @@
 // declaration de variable des elements qui vont etre recuperer dans le html
-let nom, prenom, categoriesPro, anneeNaissance;
+let nom, prenom, categoriesPro, anneeNaissance, age;
 let bouton = document.querySelector("#valider");
 // let valeurCategorie = document.querySelector("#valeurcategorie");
 let affichage = document.querySelector(".messageAffiche");
@@ -7,14 +7,8 @@ const anneeActuelle = new Date().getFullYear();
 
 // Récuperation de l'id année de span du footer
 let spanAnnee = document.querySelector("#annee");
-
-
+// Ajout de l'annee dans le footer
 spanAnnee.innerHTML = anneeActuelle;
-
-
-// Les categories professionnel 
-// On lui donne une liste des categories 
-// let categories = ["Etudiant", "Salarier"];
 
 // On creer un element option
 function optionCategories(donnee) {
@@ -46,11 +40,14 @@ function paragraphe(texte) {
     return para;
 }
 
-// La boucle va permettre d'ajouter les categories dans le code html
-// for (let index = 0; index < categories.length; index++) {
-//     valeurCategorie.appendChild(optionCategories(categories[index]));
-// }
-
+function elementRadio(listeElements){
+    for (let index = 0; index < listeElements.length; index++) {
+        if (listeElements[index].checked) { // Il va verifier si un des radio est activer
+            return listeElements[index].value;
+            // console.log(`Bonjour vous etes ${listeElements[index].value}`);
+        }
+    }return "";
+}
 
 // Declanchement de l'action affichage de l'age quand le bouton "valider" est clicker
 bouton.addEventListener("click", () => {
@@ -63,7 +60,6 @@ bouton.addEventListener("click", () => {
     prenom = document.querySelector("#prenom").value;
     anneeNaissance = document.querySelector("#anneenaissance").value;
     categoriesPro = document.querySelectorAll(".choix input[name=\"situation\"]");
-    let age;
 
     // Il va verifier si nom, prenom et annee ne sont pas vide
     if (nom !== "" && prenom !== "" && anneeNaissance !== "") {
@@ -75,26 +71,7 @@ bouton.addEventListener("click", () => {
         let regle3Et4 = `${nom} ${prenom}, vous etes ${majeur(age)}.`;
         affichage.appendChild(paragraphe(regle3Et4));
 
-        for (let index = 0; index < categoriesPro.length; index++) {
-            if (categoriesPro[index].checked) {
-                console.log(`Bonjour vous etes ${categoriesPro[index].value}`);
-            }
-            
-        }
-
-        // On vavouloir acceder a la selection des option
-        // for (let index = 0; index < valeurCategorie.length; index++) {
-        //     // On va faire en sorte
-        //     if (valeurCategorie[index].innerHTML !== "Categorie professionnel" && valeurCategorie[index].selected) {
-        //         console.log(valeurCategorie[index].innerHTML);
-        //         break;
-        //     }
-        // }
-    } else {
-        // 
+        let regleSpecial = `${nom} ${prenom}, née en ${anneeNaissance} et agé de ${age} est un ${elementRadio(categoriesPro)}`;
+        affichage.appendChild(paragraphe(regleSpecial));
     }
 });
-
-
-// Le footer
-
---
+++ js/evenement.js
@@ -0,0 +1,68 @@
+// Les evenement javascript
+let blockMessage = document.querySelector(".messageAffiche");
+
+let dateAujourdHui = new Date();
+
+let nom = document.querySelector("#nom");
+
+function paragraphe(texte) {
+    let para = document.createElement("p");
+    para.innerHTML = texte;
+    return para;
+}
+
+// Verifie 
+function afficheJournee(heure) {
+    if (heure < 12) {
+        return "Bonjour";
+    }
+    return "Bonsoir";
+}
+
+nom.addEventListener("change", (event) => {
+    if (event.target.value !== "" && event.target.value !== "\n" && event.target.value !== "\t" && event.target.value !== " ") {
+        console.log("Yo man");
+    }
+});
+
+const elementInputNom = document.querySelector("#nom");
+console.log(elementInputNom);
+
+// 2. Ecouter l'événement Change, puis exécuter le code
+elementInputNom.addEventListener("change", () => {
+    console.log(" -== J'ai entendu change :- )");
+});
+
+const elementInputPrenom = document.querySelector("#prenom");
+console.log(elementInputPrenom);
+
+let messageSalutation;
+
+elementInputPrenom.addEventListener("change", (event) => {
+    blockMessage.innerHTML = "";
+
+    if (event.target.value !== "" && event.target.value !== "\n" && event.target.value !== "\t" && event.target.value !== " ") {
+
+        console.log(dateAujourdHui.getHours());
+
+        let laJournee = afficheJournee(dateAujourdHui.getHours());
+
+        console.log("Le change est : ", event.target.value);
+
+        messageSalutation = `${laJournee} ${event.target.value}`;
+
+        blockMessage.appendChild(paragraphe(messageSalutation));
+
+        // elementMessageSalutation.textContent = messageSalutation;
+    } else {
+        alert("Rien n'a été mis !");
+    }
+});
+
+// Les dates
+
+
+
+
+
+
